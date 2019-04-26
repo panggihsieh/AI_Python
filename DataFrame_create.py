1be8b2962954b7cc3f52b8e3ded908e82705f446
panggihsieh <7624220+panggihsieh@users.noreply.github.com>
2019-04-27 00:36:31 +0800

    DataFrame 原始結構: 以Columns Dict 一欄欄呈現

---
+++ DataFrame_create.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+DataFrame 結構
+"""
+import pandas as pd
+import numpy as np
+#沒有Dict key
+l=[[i*j for j in range(1,5,1)] for i in range(1,4,1)]
+df=pd.DataFrame(l) 
+print(df);print(df.columns);print(df.index)
+
+
+#有 Dict key
+l=dict(zip(list('ABCD'),[[3 for i in range(3)] for j in range(4)]))
+df=pd.DataFrame(l)
+print(df) ; print(df.columns) ; print(df.index)
+
+
+
+
+
+        
+    
+
+
